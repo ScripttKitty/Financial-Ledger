38a8f07052fb2ae44e33ea242623c7eeec76aad3
Annie Santamaria <smaria.annie@gmail.com>
2023-10-24 11:20:50 -0400

    initial code commit

---
+++ Financial-Ledger/src/main/java/org/example/Main.java
@@ -0,0 +1,64 @@
+package org.example;
+
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //Welcome Screen
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! What would you like to do?");
+        System.out.printf("D) Add Deposit\n" +
+                                        "P) Make Payment\n" +
+                                        "L) Open Ledger\n" +
+                                        "X) Exit Application  ");
+        String userInput = scanner.nextLine();
+
+        /*
+        switch(userInput) {
+            case
+
+        }
+        *
+         */
+        //make an array list for transactions maybe call it entries?
+        //use a switch statement
+
+
+
+
+
+        try {
+
+
+
+
+
+        }
+
+
+
+        catch(Exception e){
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ Financial-Ledger/src/main/java/org/example/Transactions.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.time.LocalDate;
+
+public class Transactions {
+
+
+    //missing 2 variables
+    private String name;
+    private String vendor;
+    private double amount;
+    private LocalDate transDate;
+
+    //Parameterized constructor, missing two variables
+
+}
