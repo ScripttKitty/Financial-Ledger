8182805eeb1c884ff2f96f8b6d3d25cb61034f0a
Annie Santamaria <smaria.annie@gmail.com>
2023-10-26 12:06:25 -0400

    Fixed overwriting issue in TransactionOrganizer, removed .join()

--- Financial-Ledger/src/main/java/org/example/TransactionOrganizer.java
+++ Financial-Ledger/src/main/java/org/example/TransactionOrganizer.java
@@ -15,19 +15,17 @@ import java.util.List;
 //This class will contain methods to read from and write to the transactions.csv file
 public class TransactionOrganizer {
     //declaring List
-    private List<Transactions> transList;
+    private ArrayList<Transactions> transList;
 
     //default constructor
     public TransactionOrganizer() {
 
+        transList = new ArrayList<>();
     }
+
 
     //initializes transList and adds entries to it
     public void addTransaction(Transactions entry) {
-
-        if (transList == null) {
-            transList = new ArrayList<>();
-        }
         transList.add(entry);
         makeTransaction(transList);
 
@@ -35,20 +33,23 @@ public class TransactionOrganizer {
 
 
     //method
-    public void makeTransaction(List<Transactions> transactionsList) {
-        try (FileWriter fileWriter = new FileWriter("transactions.csv")) {
-            String info = null;
-            for (Transactions transaction : transactionsList) {
+    public void makeTransaction(ArrayList<Transactions> list) {
+        try (BufferedWriter bufWriter = new BufferedWriter ( new FileWriter("transactions.csv", true ))) {
+
+            for (Transactions transaction : list) {
                 //The join() method concatenates the elements and returns the string
-                info = String.join(" | " +
+              String  info = "\\|" +
                         transaction.getTransDate() +
                         transaction.getTransTime() +
                         transaction.getDescription() +
                         transaction.getVendor() +
-                        transaction.getAmount());
-            } //end of for loop
+                        transaction.getAmount();
 
-            fileWriter.write(info + "\n");
+                bufWriter.write(info + "\n");
+
+                System.out.println(info);
+            } //end of for loop
+            bufWriter.close();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -59,7 +60,6 @@ public class TransactionOrganizer {
 
     //May need to change else statement
     public List<Transactions> readEntries() {
-
         try (BufferedReader transFileReader = new BufferedReader(new FileReader("transactions.csv"))) {
             String transactionString;
             while ((transactionString = transFileReader.readLine()) != null) {
@@ -76,12 +76,15 @@ public class TransactionOrganizer {
                     Transactions entry = new Transactions(transDate, transTime, description, vendor, amount);
                     transList.add(entry);
                     Transactions test = new Transactions(LocalDate.now(), LocalTime.now(), "pumpkin latte", "caribou coffee", 3.50);
+                    addTransaction(test);
 
                // } else {
                     //Fix
-                    System.out.println("Invalid data provided.");
+                  //  System.out.println("Invalid data provided.");
               //  }
             } //end of while loop
+            transFileReader.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -107,6 +110,7 @@ public class TransactionOrganizer {
         for (Transactions transactions: list){
             if(transactions.getAmount() > 0){
                 System.out.println(transactions);
+
             }
         }
 
